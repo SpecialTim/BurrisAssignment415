4638a5b947517b420b76e0a9260fa3fbfd4b2d35
SpecialTim <SpecialTim96@gmail.com>
2018-04-11 18:15:14 -0500

    Redid everything

---
+++ Models/EMR.js
@@ -0,0 +1,38 @@
+var listOfEMR = {}
+function EMR (id,name,address){
+    var self = {}
+    self.id = id,
+    self.name = name,
+    self.address = address
+    return self
+}
+
+function listLength(){
+    let length = 0
+    for (i in listOfEMR){
+        length++
+    }
+    return length
+}
+
+module.exports.getListLength = () => {
+    return listLength()
+}
+
+module.exports.getEMRById = (id) => {
+    return listOfEMR[id]
+} 
+
+module.exports.createEMR = (name,address) => {
+    let id = listLength()
+    var emr = new EMR(id,name,address)
+    listOfEMR[emr.id] = emr
+}
+
+module.exports.deleteEMR = (emr) => {
+    delete listOfEMR[emr.id]
+}
+
+module.exports.getList = () => {
+    return listOfEMR
+}
--- server.js
+++ server.js
@@ -1,18 +1,45 @@
 var express = require('express');
 var chalk = require('chalk');
 var app = express();
-var router = express.Router();
+const bodyparser = require('body-parser')
+const PORT = process.env.PORT || 5000
+const EMR = require('./Models/EMR')
+var data = {
+    emrWasCreated: false
+}
 
-router.get('rest/emr', function(req, res) {
-    res.status(200).send('all of emr details');
-});
-router.get('rest/emr/id', function(req, res) {
-    res.status(200).send('emr id');
-});
-router.post('rest/emr', function(req, res) {
-    res.status(200).send('Hello world');
-});
+app.use(bodyparser.urlencoded({extended: true}))
+app.use(express.static(__dirname + '/client/'))
 
-app.use('/rest', router);
+app.set('views', __dirname + '/views/')
+app.set('view engine' , 'ejs')
 
-app.listen(process.env.PORT || 5000)
+app.get('/', (req,res) =>{
+    res.render('index')
+})
+
+app.get('/rest/view/', (req,res) => {
+    data.emrList = EMR.getList()
+    res.render('ViewAllEMR', data)
+})
+
+app.get('/rest/emr', (req, res) => {
+    data.emrWasCreated = false
+    res.render('CreateEMR.ejs', data)
+})
+
+
+
+app.post('/rest/emr/', (req,res) => {
+    let name = req.body.name
+    let address = req.body.address
+    EMR.createEMR(name,address)
+    let emr = EMR.getEMRById(EMR.getListLength() - 1)
+    data.emrWasCreated = true
+    data.emr = emr
+    res.render('CreateEMR.ejs', data)
+})
+
+app.listen(PORT, () =>{
+    console.log(`Listening on ${PORT}`)
+})
